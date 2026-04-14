6ead10b8162ef8123ec60d10e21e34b98b8124f6
Md. Rajiul <109409299+rajiul93@users.noreply.github.com>
2026-04-14 09:53:12 +0600

    feat: refactor ResumePage to use ResumePageClient and implement tab navigation for templates

--- src/app/dashboard/contents/resume/page.tsx
+++ src/app/dashboard/contents/resume/page.tsx
@@ -1,47 +1,11 @@
-'use client';
-
-import React, { useEffect } from 'react';
-import dynamic from 'next/dynamic';
-import { useSearchParams, useRouter } from 'next/navigation';
-import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
-const TemplateOne = dynamic(() => import('./template-one'), {
-  ssr: false,
-});
-
-const TemplateTwo = dynamic(() => import('./template-two'), {
-  ssr: false,
-});
+import { Suspense } from 'react';
+import ResumePageClient from './resume-client';
 
 const ResumePage = () => {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-
-  const templete = searchParams.get('templete') || 'one';
-
-  // Sync tab change with URL
-  const handleTabChange = (value: string) => {
-    router.push(`?templete=${value}`);
-  };
-
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-2xl font-bold mb-6">Resume</h1>
-
-      <Tabs className="" value={templete} onValueChange={handleTabChange}>
-        <TabsList className="mb-6">
-          <TabsTrigger value="one">Template One</TabsTrigger>
-          <TabsTrigger value="two">Template Two</TabsTrigger>
-        </TabsList>
-
-        <TabsContent value="one">
-          <TemplateOne />
-        </TabsContent>
-
-        <TabsContent value="two">
-          <TemplateTwo />
-        </TabsContent>
-      </Tabs>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResumePageClient />;
+    </Suspense>
   );
 };
 
